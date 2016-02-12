00bd51a2a22641682aa0330cfc98eacb94e10a3f
José Cortez <Jospcortez@gmail.com>
2016-02-12 14:36:52 +0000

    Angular Accordion is now working!

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,8 +1,15 @@
-var app = angular.module('BookItineraries', []);
+var app = angular.module('BookItineraries', ['ui.bootstrap']);
 
-app.controller('MainCtrl', function ($scope) {
-    $scope.books = [
-        'All the Light We Cannot See',
-        'Over the Edge of the World: Magellan\'s Terrifying Circumnavegation of the Globe'
-    ];
+app.controller('MainCtrl', function($scope) {
+  $scope.books = [
+    'All the Light We Cannot See',
+    'Over the Edge of the World: Magellan\'s Terrifying Circumnavegation of the Globe'
+  ];
+
+  $scope.status = {
+    isFirstOpen: true,
+    openStatus: [true],
+    oneAtATime: true
+  };
+
 });
