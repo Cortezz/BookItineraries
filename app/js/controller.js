47260a2e8ac25ceb7ee5312eef8151ebb05208c0
José Cortez <Jospcortez@gmail.com>
2016-02-12 20:09:53 +0000

    Need an HTTP GET request to some API regarding book information

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,15 +1,42 @@
 var app = angular.module('BookItineraries', ['ui.bootstrap']);
 
+
+
 app.controller('MainCtrl', function($scope) {
   $scope.books = [
-    'All the Light We Cannot See',
-    'Over the Edge of the World: Magellan\'s Terrifying Circumnavegation of the Globe'
-  ];
+    {
+      "title" : "All the Light We Cannot See",
+      "ISBN" : "1476746583"
+    },
+    {
+      "title": 'Over the Edge of the World: Magellan\'s Terrifying Circumnavegation of the Globe',
+      "ISBN" : "006093638X"
+    }];
 
   $scope.status = {
     isFirstOpen: true,
     openStatus: [true],
     oneAtATime: true
   };
+
+  $scope.getBookInfo = function () {
+       BookInfoService.get({ q: $scope.searchTerm }, function (response) {
+
+           $scope.bookResults = response.items;
+           $scope.orderProp = 'volumeInfo.title';
+
+       });
+   }
+
+});
+
+app.controller("bookInfo", function($scope, $http) {
+  /*$http.get(' http://openlibrary.org/api/volumes/brief/isbn/1476746583.json').success(function(data) {
+       $scope.bookInfo = data;
+       console.Log("YO!");
+   });*/
+   $http.get('https://api.github.com/users/angular/repos').success(function(data) {
+     $scope.gitRepos = data;
+   });
 
 });
