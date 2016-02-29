c84d99949dc7c73ec8b9a83f82fa6b054942d93e
José Cortez <Jospcortez@gmail.com>
2016-02-29 12:30:01 +0000

    Now using custom Google Maps icons

--- public/js/drawItinerary.js
+++ public/js/drawItinerary.js
@@ -4,10 +4,12 @@ function drawMap(map, bookItinerary) {
   var currentCharacter;
   var markers = [];
   var coords;
+  var itineraryLength = [];
 
   for (i = 0; i < bookItinerary.length; i++) {
     currentCharacter = bookItinerary[i];
     path = [];
+    itineraryLength[i] = currentCharacter.coords.length;
 
     for (j = 0; j < currentCharacter.coords.length; j++) {
       coords = new google.maps.LatLng(currentCharacter.coords[j].lat, currentCharacter.coords[j].long);
@@ -22,37 +24,40 @@ function drawMap(map, bookItinerary) {
     var polyline = new google.maps.Polyline({
       path: path,
       strokeColor: generateRandomColor(),
-      strokeWeight: 5,
+      strokeWeight: 3,
       strokeOpacity: 0.8
     })
     polyline.setMap(map);
   }
-  drawMarkers(markers, map);
+  drawMarkers(markers, map, itineraryLength);
 }
 
 function generateRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  return 'rgb(' + (Math.floor(Math.random() * 256)) + ','
+  + (Math.floor(Math.random() * 256)) + ','
+  + (Math.floor(Math.random() * 256)) + ')';
 }
 
 
-function drawMarkers(markers, map) {
-  var i;
+function drawMarkers(markers, map, itineraryLength) {
+  var i,j,markerCounter;
   var marker;
   var infowindow = new google.maps.InfoWindow();
 
-  for (i = 0; i < markers.length; i++) {
+  for (i = 0, j=0, markerCounter=1; i < markers.length; i++, markerCounter++) {
 
     marker = new google.maps.Marker({
       position: markers[i].coords,
       title: "Character",
-      icon: "../img/marker-24.svg",
+      icon: "https://7c29ac479ebd9f2e21b06584476b397450a02067-www.googledrive.com/host/0BwyVoAL8Xtc-VllZUzFNNDcyclU/number_"+markerCounter+"%20copy.png",
       map: map
     });
+
+    if (markerCounter==(itineraryLength[j]))
+    {
+      markerCounter = 0;
+      j++;
+    }
 
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
       return function() {
